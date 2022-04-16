e000705fe7e5f9ac8f32ef2507855f76c53a3a4e
Ken Shimizu <kenshimizu88@gmail.com>
2022-04-16 12:00:17 -1000

    Event Flow

--- app/src/main/java/com/example/androidkata/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidkata/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.androidkata.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -14,12 +15,19 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     val viewModel by viewModels<MainViewModel>()
     lateinit var rvTodoList: RecyclerView
+    lateinit var loadNew: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadNew = findViewById(R.id.loadNew)
         rvTodoList = findViewById(R.id.rvTodoList)
+
+        loadNew.setOnClickListener {
+            viewModel.performAction(Event.LoadNewData)
+        }
+
         rvTodoList.adapter = TodoListAdapter(
             viewModel.state.value.todos,
             viewModel::onChecked
--- app/src/main/java/com/example/androidkata/ui/MainViewModel.kt
+++ app/src/main/java/com/example/androidkata/ui/MainViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.androidkata.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 data class Todo(
     val title: String,
@@ -10,14 +15,43 @@ data class Todo(
 
 data class State(val todos: List<Todo>)
 
+sealed interface Event {
+    object LoadNewData: Event
+}
+
 class MainViewModel: ViewModel() {
     var state: MutableStateFlow<State> = MutableStateFlow(State(listOf()))
+    val events: MutableSharedFlow<Event> = MutableSharedFlow(
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
 
     init {
         state.tryEmit(
             State(listOf(
                 Todo("New Todo", false),
                 Todo("Old Todo", true)
+            ))
+        )
+
+        viewModelScope.launch {
+            events.collect {
+                when(it) {
+                    Event.LoadNewData -> loadNew()
+                }
+            }
+        }
+    }
+
+    fun performAction(action: Event) {
+        events.tryEmit(action)
+    }
+
+    private fun loadNew() {
+        state.tryEmit(
+            State(listOf(
+                Todo("LOADED NEW", false),
+                Todo("SUPER NEW STUFF", true)
             ))
         )
     }
